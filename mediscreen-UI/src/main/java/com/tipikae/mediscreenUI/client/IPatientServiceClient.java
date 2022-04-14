390f35cd569424e1abd94012e16f40e91ab898ed
tipikae <gilles.bd@free.fr>
2022-04-14 16:10:00 +0200

    adding client methods

--- mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/client/IPatientServiceClient.java
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/client/IPatientServiceClient.java
@@ -9,6 +9,10 @@ import com.tipikae.mediscreenUI.dto.NewPatientDTO;
 import com.tipikae.mediscreenUI.dto.UpdatePatientDTO;
 import com.tipikae.mediscreenUI.model.Patient;
 
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
 /**
  * Feign client for Patient Service.
  * @author tipikae
@@ -17,9 +21,36 @@ import com.tipikae.mediscreenUI.model.Patient;
  */
 public interface IPatientServiceClient {
 
-	
+	/**
+	 * Get patients list.
+	 * @return List
+	 */
+	@RequestLine("GET /")
 	List<Patient> getPatients();
-	Patient getPatient(int id);
+	
+	/**
+	 * Get a patient.
+	 * @param id int
+	 * @return Patient
+	 */
+	@RequestLine("GET /{id}")
+	Patient getPatient(@Param("id") int id);
+	
+	/**
+	 * Add new patient.
+	 * @param newPatientDTO NewPatientDTO
+	 * @return Patient
+	 */
+	@RequestLine("POST /")
+    @Headers("Content-Type: application/json")
 	Patient addPatient(NewPatientDTO newPatientDTO);
-	void updatePatient(int id, UpdatePatientDTO updatePatientDTO);
+	
+	/**
+	 * Update a patient.
+	 * @param id int
+	 * @param updatePatientDTO UpdatePatientDTO
+	 */
+	@RequestLine("PUT /{id}")
+    @Headers("Content-Type: application/json")
+	void updatePatient(@Param("id") int id, UpdatePatientDTO updatePatientDTO);
 }
