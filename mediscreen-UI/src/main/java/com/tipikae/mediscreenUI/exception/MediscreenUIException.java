6027fa70d18197f9a95dd8670b69259d3df890a5
tipikae <gilles.bd@free.fr>
2022-04-14 10:47:06 +0200

    adding parent exception

---
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/exception/MediscreenUIException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tipikae.mediscreenUI.exception;
+
+/**
+ * Parent exception for all application's exceptions.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class MediscreenUIException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public MediscreenUIException(String message) {
+		super(message);
+	}
+
+}
