102b0e61a0ee310a3457eb6d439cc8876458e626
tipikae <gilles.bd@free.fr>
2022-04-14 06:58:14 +0200

    removing to_date function

--- patient-service/src/main/resources/db/changelog/db.changelog-0.0.1.sql
+++ patient-service/src/main/resources/db/changelog/db.changelog-0.0.1.sql
@@ -17,10 +17,10 @@ CREATE TABLE patient (
 --changeset tipikae:2
 /*insert values into patient table*/
 INSERT INTO patient(family, given, dob, sex, address, phone) 
-	VALUES('TestNone', 'Test', TO_DATE('1966-12-31', 'YYYY-MM-DD'), 'F', '1 Brookside St', '100-222-3333');
+	VALUES('TestNone', 'Test', '1966-12-31', 'F', '1 Brookside St', '100-222-3333');
 INSERT INTO patient(family, given, dob, sex, address, phone) 
-	VALUES('TestBorderline', 'Test', TO_DATE('1945-06-24', 'YYYY-MM-DD'), 'M', '2 High St', '200-333-4444');
+	VALUES('TestBorderline', 'Test', '1945-06-24', 'M', '2 High St', '200-333-4444');
 INSERT INTO patient(family, given, dob, sex, address, phone) 
-	VALUES('TestInDanger', 'Test', TO_DATE('2004-06-18', 'YYYY-MM-DD'), 'M', '3 Club Road', '300-444-5555');
+	VALUES('TestInDanger', 'Test', '2004-06-18', 'M', '3 Club Road', '300-444-5555');
 INSERT INTO patient(family, given, dob, sex, address, phone) 
-	VALUES('TestEarlyOnset', 'Test', TO_DATE('2002-06-28', 'YYYY-MM-DD'), 'F', '4 Valley Dr', '400-555-6666');
+	VALUES('TestEarlyOnset', 'Test', '2002-06-28', 'F', '4 Valley Dr', '400-555-6666');
