2015a533b873b22fb711afa273da2b567955373a
tipikae <gilles.bd@free.fr>
2022-04-15 07:03:01 +0200

    adding validation

--- patient-service/src/main/java/com/tipikae/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/tipikae/patientservice/model/Patient.java
@@ -10,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,10 +33,16 @@ public class Patient {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@NotBlank
 	private String family;
+	@NotBlank
 	private String given;
+	@NotNull
 	private LocalDate dob;
+	@NotNull
 	private char sex;
+	@NotBlank
 	private String address;
+	@NotBlank
 	private String phone;
 }
