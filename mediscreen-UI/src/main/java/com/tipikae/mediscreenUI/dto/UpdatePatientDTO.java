be036f19f7d24653806e06e5ec885bf3fcd8544f
tipikae <gilles.bd@free.fr>
2022-04-15 20:08:53 +0200

    adding json de/serialize

--- mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/dto/UpdatePatientDTO.java
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/dto/UpdatePatientDTO.java
@@ -8,6 +8,10 @@ import java.time.LocalDate;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,14 +29,21 @@ public class UpdatePatientDTO {
 
 	@NotBlank(message = "Lastname must not be empty.")
 	private String family;
+	
 	@NotBlank(message = "Firstname must not be empty.")
 	private String given;
+	
 	@NotNull(message = "Birthdate must not be empty.")
+	@JsonSerialize(using = LocalDateSerializer.class)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private LocalDate dob;
+	
 	@NotNull(message = "Sex must not be empty.")
 	private char sex;
+	
 	@NotBlank(message = "Address must not be empty.")
 	private String address;
+	
 	@NotBlank(message = "Phone number must not be empty.")
 	private String phone;
 
