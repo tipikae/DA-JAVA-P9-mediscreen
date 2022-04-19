7c17a06609b0e1ae76033a96f32bb02d7559bd2d
tipikae <gilles.bd@free.fr>
2022-04-19 11:02:56 +0200

    adding dtos

---
+++ patient-service/src/main/java/com/tipikae/patientservice/dto/UpdatePatientDTO.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.tipikae.patientservice.dto;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Update patient DTO.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePatientDTO {
+
+	@NotBlank(message = "Lastname must not be empty.")
+	private String family;
+	
+	@NotBlank(message = "Firstname must not be empty.")
+	private String given;
+	
+	@NotNull(message = "Birthdate must not be empty.")
+	@JsonSerialize(using = LocalDateSerializer.class)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Past
+	private LocalDate dob;
+	
+	@NotNull(message = "Sex must not be empty.")
+	private char sex;
+	
+	@NotBlank(message = "Address must not be empty.")
+	private String address;
+	
+	@NotBlank(message = "Phone number must not be empty.")
+	private String phone;
+}
