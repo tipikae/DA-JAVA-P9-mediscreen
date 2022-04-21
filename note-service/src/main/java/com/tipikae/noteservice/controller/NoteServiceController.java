6eb254bea01d197e9983d1887e6fd44be03c4a99
tipikae <gilles.bd@free.fr>
2022-04-21 19:18:59 +0200

    adding controller

---
+++ note-service/src/main/java/com/tipikae/noteservice/controller/NoteServiceController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.tipikae.noteservice.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Note controller.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@RestController
+@Validated
+public class NoteServiceController {
+
+}
