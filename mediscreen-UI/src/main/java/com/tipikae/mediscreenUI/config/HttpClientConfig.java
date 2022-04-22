9f9c4bf1889c73027917e895b11273ff5fd372db
tipikae <gilles.bd@free.fr>
2022-04-22 11:05:25 +0200

    adding note service client config

--- mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/config/HttpClientConfig.java
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/config/HttpClientConfig.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.tipikae.mediscreenUI.client.INoteServiceClient;
 import com.tipikae.mediscreenUI.client.IPatientServiceClient;
-import com.tipikae.mediscreenUI.exception.MyFeignErrorDecoder;
+import com.tipikae.mediscreenUI.errorDecoder.NoteServiceErrorDecoder;
+import com.tipikae.mediscreenUI.errorDecoder.PatientServiceErrorDecoder;
 
 import feign.Feign;
 import feign.Logger;
@@ -25,6 +27,7 @@ import feign.jackson.JacksonEncoder;
 public class HttpClientConfig {
 	
 	private static final String PATIENT_SERVICE_URL = "/patient-service";
+	private static final String NOTE_SERVICE_URL = "/note-service";
 
 	@Value(value = "${proxy.url:}")
 	private String proxyUrl;
@@ -34,8 +37,17 @@ public class HttpClientConfig {
 		return Feign.builder()
 				.encoder(new JacksonEncoder())
 				.decoder(new JacksonDecoder())
-				.errorDecoder(new MyFeignErrorDecoder())
+				.errorDecoder(new PatientServiceErrorDecoder())
 				.target(IPatientServiceClient.class, proxyUrl + PATIENT_SERVICE_URL);
+	}
+	
+	@Bean
+	public INoteServiceClient getNoteServiceClient() {
+		return Feign.builder()
+				.encoder(new JacksonEncoder())
+				.decoder(new JacksonDecoder())
+				.errorDecoder(new NoteServiceErrorDecoder())
+				.target(INoteServiceClient.class, proxyUrl + NOTE_SERVICE_URL);
 	}
 	
 	@Bean
