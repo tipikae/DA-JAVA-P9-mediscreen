5cde5c2eb7bddbe5c35238491a627efeb7708c86
tipikae <gilles.bd@free.fr>
2022-04-22 11:07:57 +0200

    creating note service feign client

---
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/client/INoteServiceClient.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package com.tipikae.mediscreenUI.client;
+
+import java.util.List;
+
+import com.tipikae.mediscreenUI.dto.NewNoteDTO;
+import com.tipikae.mediscreenUI.dto.UpdateNoteDTO;
+import com.tipikae.mediscreenUI.exception.BadRequestException;
+import com.tipikae.mediscreenUI.exception.HttpClientException;
+import com.tipikae.mediscreenUI.exception.NoteNotFoundException;
+import com.tipikae.mediscreenUI.model.Note;
+
+import feign.Headers;
+import feign.RequestLine;
+
+/**
+ * Note service Feign client.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public interface INoteServiceClient {
+
+	/**
+	 * Get all patient's notes.
+	 * @param patId long
+	 * @return List
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
+	@RequestLine("GET /notes/search?patId={patId}")
+	List<Note> getPatientNotes(long patId) 
+			throws BadRequestException, HttpClientException;
+
+	/**
+	 * Get a note.
+	 * @param id String
+	 * @return Note
+	 * @throws NoteNotFoundException
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
+	@RequestLine("GET /notes/id/{id}")
+	Note getNote(String id) 
+			throws NoteNotFoundException, BadRequestException, HttpClientException;
+	
+	/**
+	 * Add a note.
+	 * @param newNoteDTO NewNoteDTO
+	 * @return Note
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
+	@RequestLine("POST /notes/")
+    @Headers("Content-Type: application/json")
+	Note addNote(NewNoteDTO newNoteDTO) 
+			throws BadRequestException, HttpClientException;
+
+	/**
+	 * Update a note.
+	 * @param id String
+	 * @param updateNoteDTO
+	 * @throws NoteNotFoundException
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
+	@RequestLine("PUT /notes/{id}")
+    @Headers("Content-Type: application/json")
+	void updateNote(String id, UpdateNoteDTO updateNoteDTO) 
+			throws NoteNotFoundException, BadRequestException, HttpClientException;
+
+	/**
+	 * Delete a note.
+	 * @param id String
+	 * @throws NoteNotFoundException
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
+	@RequestLine("DELETE /notes/{id}")
+	void deleteNote(String id) 
+			throws NoteNotFoundException, BadRequestException, HttpClientException;
+}
