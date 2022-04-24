542ab73b94b1f8370f834c877d465683437ca0a7
tipikae <gilles.bd@free.fr>
2022-04-24 08:59:46 +0200

    enable mongock

--- note-service/src/main/java/com/tipikae/noteservice/NoteServiceApplication.java
+++ note-service/src/main/java/com/tipikae/noteservice/NoteServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.mongock.runner.springboot.EnableMongock;
 
+
+@EnableMongock
 @EnableEurekaClient
 @SpringBootApplication
 public class NoteServiceApplication {
