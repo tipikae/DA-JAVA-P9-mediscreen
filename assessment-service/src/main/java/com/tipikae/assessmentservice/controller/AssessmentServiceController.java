5a67d00df61c9d35d4efbdac83363c326de49c01
tipikae <gilles.bd@free.fr>
2022-04-25 16:03:15 +0200

    implementing and testing controller

--- assessment-service/src/main/java/com/tipikae/assessmentservice/controller/AssessmentServiceController.java
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/controller/AssessmentServiceController.java
@@ -3,11 +3,14 @@
  */
 package com.tipikae.assessmentservice.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tipikae.assessmentservice.dto.AssessmentByFamilyDTO;
 import com.tipikae.assessmentservice.dto.AssessmentByIdDTO;
 import com.tipikae.assessmentservice.dto.AssessmentDTO;
+import com.tipikae.assessmentservice.exception.BadRequestException;
+import com.tipikae.assessmentservice.exception.HttpClientException;
+import com.tipikae.assessmentservice.exception.PatientNotFoundException;
 import com.tipikae.assessmentservice.service.IAssessmentServiceService;
 
 /**
@@ -36,14 +42,39 @@ public class AssessmentServiceController {
 	@Autowired
 	private IAssessmentServiceService assessmentService;
 	
+	/**
+	 * Assess by patient id.
+	 * @param assessmentByIdDTO AssessmentByIdDTO
+	 * @return ResponseEntity
+	 * @throws PatientNotFoundException
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
 	@PostMapping(value = "/id", consumes = {"application/json"})
-	public ResponseEntity<AssessmentDTO> assessById(@RequestBody @Valid AssessmentByIdDTO dto) {
-		return null;
+	public ResponseEntity<AssessmentDTO> assessById(
+			@RequestBody @Valid AssessmentByIdDTO assessmentByIdDTO) 
+					throws PatientNotFoundException, BadRequestException, HttpClientException {
+		LOGGER.info("assessById: patId=" + assessmentByIdDTO.getPatId());
+		AssessmentDTO assessmentDTO = assessmentService.assessDiabetesById(assessmentByIdDTO);
+		
+		return new ResponseEntity<AssessmentDTO>(assessmentDTO, HttpStatus.OK);
 	}
 	
+	/**
+	 * Assess by family name.
+	 * @param assessmentByFamilyDTO AssessmentByFamilyDTO
+	 * @return ResponseEntity
+	 * @throws BadRequestException
+	 * @throws HttpClientException
+	 */
 	@PostMapping(value = "/familyName", consumes = {"application/json"})
-	public ResponseEntity<AssessmentDTO> assessByFamilyName(@RequestBody @Valid AssessmentByFamilyDTO dto) {
-		return null;
+	public ResponseEntity<List<AssessmentDTO>> assessByFamilyName(
+			@RequestBody @Valid AssessmentByFamilyDTO assessmentByFamilyDTO) 
+					throws BadRequestException, HttpClientException {
+		LOGGER.info("assessByFamilyName: family=" + assessmentByFamilyDTO.getFamilyName());
+		List<AssessmentDTO> assessmentDTOs = assessmentService.assessDiabetesByFamilyName(assessmentByFamilyDTO);
+		
+		return new ResponseEntity<List<AssessmentDTO>>(assessmentDTOs, HttpStatus.OK);
 	}
 
 }
--- assessment-service/src/test/java/com/tipikae/assessmentservice/unit/AssessmentServiceControllerTest.java
+++ assessment-service/src/test/java/com/tipikae/assessmentservice/unit/AssessmentServiceControllerTest.java
@@ -1,14 +1,29 @@
 package com.tipikae.assessmentservice.unit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.tipikae.assessmentservice.controller.AssessmentServiceController;
+import com.tipikae.assessmentservice.dto.AssessmentByFamilyDTO;
+import com.tipikae.assessmentservice.dto.AssessmentByIdDTO;
+import com.tipikae.assessmentservice.dto.AssessmentDTO;
+import com.tipikae.assessmentservice.exception.BadRequestException;
+import com.tipikae.assessmentservice.exception.PatientNotFoundException;
 import com.tipikae.assessmentservice.service.IAssessmentServiceService;
 
 @WebMvcTest(controllers = AssessmentServiceController.class)
@@ -19,30 +34,79 @@ class AssessmentServiceControllerTest {
 	
 	@MockBean
 	private IAssessmentServiceService assessmentService;
+	
+	private static final String ROOT = "/assess";
+	
+	private static long patId = 1L;
+	private static String message = "message";
+	private static String family = "family";
 
 	@Test
-	void assessByIdReturns200AndAssessmentWhenOk() {
-		
+	void assessByIdReturns200AndAssessmentWhenOk() throws Exception {
+		when(assessmentService.assessDiabetesById(any(AssessmentByIdDTO.class)))
+			.thenReturn(new AssessmentDTO(message));
+		mockMvc.perform(post(ROOT + "/id")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"patId\": " + patId + "}"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message", is(message)));
 	}
 
 	@Test
-	void assessByIdReturns404WhenPatientNotFound() {
-		
+	void assessByIdReturns404WhenPatientNotFound() throws Exception {
+		doThrow(PatientNotFoundException.class)
+			.when(assessmentService).assessDiabetesById(any(AssessmentByIdDTO.class));
+		mockMvc.perform(post(ROOT + "/id")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"patId\": " + patId + "}"))
+			.andExpect(status().is(404));
 	}
 
 	@Test
-	void assessByIdReturns400WhenBadRequest() {
-		
+	void assessByIdReturns400WhenBadRequest() throws Exception {
+		doThrow(BadRequestException.class)
+			.when(assessmentService).assessDiabetesById(any(AssessmentByIdDTO.class));
+		mockMvc.perform(post(ROOT + "/id")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"patId\": " + patId + "}"))
+			.andExpect(status().is(400));
 	}
 
 	@Test
-	void assessByFamilyNameReturns200AndListWhenOk() {
-		
+	void assessByIdReturns400WhenBadField() throws Exception {
+		mockMvc.perform(post(ROOT + "/id")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"patId\": 0}"))
+			.andExpect(status().is(400));
 	}
 
 	@Test
-	void assessByFamilyNameReturns400WhenBadRequest() {
-		
+	void assessByFamilyNameReturns200AndListWhenOk() throws Exception {
+		when(assessmentService.assessDiabetesByFamilyName(any(AssessmentByFamilyDTO.class)))
+			.thenReturn(new ArrayList<>());
+		mockMvc.perform(post(ROOT + "/familyName")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"familyName\": \"" + family + "\"}"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", isA(ArrayList.class)));
+	}
+
+	@Test
+	void assessByFamilyNameReturns400WhenBadRequest() throws Exception {
+		doThrow(BadRequestException.class)
+			.when(assessmentService).assessDiabetesByFamilyName(any(AssessmentByFamilyDTO.class));
+		mockMvc.perform(post(ROOT + "/familyName")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"familyName\": \"" + family + "\"}"))
+			.andExpect(status().is(400));
+	}
+
+	@Test
+	void assessByFamilyNameReturns400WhenBadField() throws Exception {
+		mockMvc.perform(post(ROOT + "/familyName")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"familyName\": \"\"}"))
+			.andExpect(status().is(400));
 	}
 
 }
