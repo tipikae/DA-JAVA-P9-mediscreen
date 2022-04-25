45d946305c2e03ba44e4dac9ebc7863e01c66db5
tipikae <gilles.bd@free.fr>
2022-04-25 16:03:42 +0200

    adding controller exception handler

---
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/exception/ControllerException.java
@@ -0,0 +1,28 @@
+package com.tipikae.assessmentservice.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * Controller exception.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@Data
+public class ControllerException implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private int code;
+	private String message;
+	private Date timestamp;
+
+	public ControllerException(int code, String message) {
+		this.code = code;
+		this.message = message;
+		this.timestamp = new Date();
+	}
+}
