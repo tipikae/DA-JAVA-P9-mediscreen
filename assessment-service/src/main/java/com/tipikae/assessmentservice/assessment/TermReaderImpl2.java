f14788e85262ff32d3c19388c0c9a8b29c75adba
tipikae <gilles.bd@free.fr>
2022-05-02 16:07:03 +0200

    adding ITermReader implementation

---
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/assessment/TermReaderImpl2.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.tipikae.assessmentservice.assessment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.tipikae.assessmentservice.exception.TermReaderException;
+import com.tipikae.assessmentservice.repository.ITriggerRepository;
+
+/**
+ * Terms reader.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@Component
+public class TermReaderImpl2 implements ITermReader {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TermReaderImpl.class);
+	
+	@Autowired
+	private ITriggerRepository triggerRepository;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<String> getTerms() throws TermReaderException {
+		LOGGER.debug("getTerms from repository");
+		return triggerRepository.findAll().stream()
+				.map(trigger -> trigger.getTerm())
+				.collect(Collectors.toList());
+	}
+
+}
