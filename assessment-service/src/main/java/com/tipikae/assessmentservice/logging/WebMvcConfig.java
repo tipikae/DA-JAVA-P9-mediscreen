2585866ec3c60e0b691b60afcd3431777ab53bc1
tipikae <gilles.bd@free.fr>
2022-05-07 14:34:10 +0200

    only one package for logging

--- assessment-service/src/main/java/com/tipikae/assessmentservice/logging/WebMvcConfig.java
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/logging/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.tipikae.assessmentservice.config;
+package com.tipikae.assessmentservice.logging;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
