534239e8ee3cf9c9bb9ed4a3cbe7b0af9fba9d67
tipikae <gilles.bd@free.fr>
2022-05-07 16:50:32 +0200

    adding boolean operator enum

---
+++ assessment-service/src/test/java/com/tipikae/assessmentservice/unit/risk2/BooleanOperatorTest.java
@@ -0,0 +1,36 @@
+package com.tipikae.assessmentservice.unit.risk2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.tipikae.assessmentservice.risk2.BooleanOperator;
+
+class BooleanOperatorTest {
+
+	@Test
+	void apply() {
+		assertTrue(BooleanOperator.AND.apply(true, true));
+		assertFalse(BooleanOperator.AND.apply(true, false));
+		assertFalse(BooleanOperator.AND.apply(false, false));
+
+		assertTrue(BooleanOperator.OR.apply(true, true));
+		assertTrue(BooleanOperator.OR.apply(false, true));
+		assertFalse(BooleanOperator.OR.apply(false, false));
+	}
+	
+	@Test
+	void valueOf() {
+		String and = "AND";
+		String or = "OR";
+		
+		assertTrue(BooleanOperator.valueOfOperator(and).apply(true, true));
+		assertFalse(BooleanOperator.valueOfOperator(and).apply(true, false));
+		assertFalse(BooleanOperator.valueOfOperator(and).apply(false, false));
+
+		assertTrue(BooleanOperator.valueOfOperator(or).apply(true, true));
+		assertTrue(BooleanOperator.valueOfOperator(or).apply(false, true));
+		assertFalse(BooleanOperator.valueOfOperator(or).apply(false, false));
+	}
+
+}
