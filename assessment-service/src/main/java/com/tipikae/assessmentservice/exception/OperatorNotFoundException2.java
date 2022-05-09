50f1c70c07d4513c7d9b18df57ea19f537403e34
tipikae <gilles.bd@free.fr>
2022-05-09 07:37:36 +0200

    adding new exceptions

---
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/exception/OperatorNotFoundException2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tipikae.assessmentservice.exception;
+
+/**
+ * Operator not found exception.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class OperatorNotFoundException2 extends AssessmentServiceException {
+
+	private static final long serialVersionUID = 1L;
+
+	public OperatorNotFoundException2(String message) {
+		super(message);
+	}
+
+}
