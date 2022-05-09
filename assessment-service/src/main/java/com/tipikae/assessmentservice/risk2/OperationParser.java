dc534fc1f5c7d6d4cd8a9136f86b5cd871d1d07a
tipikae <gilles.bd@free.fr>
2022-05-09 09:09:17 +0200

    adding operationParser

---
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/risk2/OperationParser.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.tipikae.assessmentservice.risk2;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Operation parser.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@Component
+public class OperationParser {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(OperationParser.class);
+	private static final String MODEL_ELEMENTS = "%s\\.([a-zA-Z0-9]+)\\s?([=<>!]{1,2})\\s?([a-zA-Z0-9]+)";
+	private static final String METHOD_ELEMENTS = "([a-zA-Z0-9]+)\\s?([=<>!]{1,2})\\s?([a-zA-Z0-9]+)";
+
+	/**
+	 * Get elements of an operation with model.
+	 * @param model char
+	 * @param operation String
+	 * @return List
+	 */
+	public List<String> getModelElements(char model, String operation) {
+		LOGGER.debug("getModelElements: model=" + model + ", expression=" + operation);
+		String pattern = String.format(MODEL_ELEMENTS, model);
+		Matcher matcher = Pattern.compile(pattern).matcher(operation);
+		
+		return getElements(matcher);
+	}
+
+	/**
+	 * Get elements of an operation with method.
+	 * @param operation String
+	 * @return List
+	 */
+	public List<String> getMethodElements(String operation) {
+		LOGGER.debug("getMethodElements: expression=" + operation);
+		Matcher matcher = Pattern.compile(METHOD_ELEMENTS).matcher(operation);
+		
+		return getElements(matcher);
+	}
+	
+	// return a list of found elements or an empty list.
+	private List<String> getElements(Matcher matcher) {
+		if(matcher.find()) {
+    		int count = matcher.groupCount();
+		    
+    		return IntStream.rangeClosed(1, count)
+		        .mapToObj(i -> matcher.group(i))
+		        .collect(Collectors.toList());
+		}
+		
+		return List.of();
+	}
+
+}
---
+++ assessment-service/src/test/java/com/tipikae/assessmentservice/unit/risk2/OperationParserTest.java
@@ -0,0 +1,44 @@
+package com.tipikae.assessmentservice.unit.risk2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.tipikae.assessmentservice.risk2.OperationParser;
+
+class OperationParserTest {
+	
+	private OperationParser operationParser = new OperationParser();
+	
+	private static final String RIGHT_MODEL_OPERATION = "P.age < 30";
+	private static final String BAD_MODEL_OPERATION = "M.age < 30";
+	private static final String RIGHT_METHOD_OPERATION = "count = 2";
+	private static final String BAD_METHOD_OPERATION = "count$ = 2";
+
+	@Test
+	void getModelElementsReturnsElementsWhenFound() {
+		assertEquals(3, operationParser.getModelElements('P', RIGHT_MODEL_OPERATION).size());
+		assertEquals("age", operationParser.getModelElements('P', RIGHT_MODEL_OPERATION).get(0));
+		assertEquals("<", operationParser.getModelElements('P', RIGHT_MODEL_OPERATION).get(1));
+		assertEquals("30", operationParser.getModelElements('P', RIGHT_MODEL_OPERATION).get(2));
+	}
+
+	@Test
+	void getModelElementsReturnsEmptyListWhenNotFound() {
+		assertTrue(operationParser.getModelElements('P', BAD_MODEL_OPERATION).isEmpty());
+	}
+
+	@Test
+	void getMethodElementsReturnsElementsWhenFound() {
+		assertEquals(3, operationParser.getMethodElements(RIGHT_METHOD_OPERATION).size());
+		assertEquals("count", operationParser.getMethodElements(RIGHT_METHOD_OPERATION).get(0));
+		assertEquals("=", operationParser.getMethodElements(RIGHT_METHOD_OPERATION).get(1));
+		assertEquals("2", operationParser.getMethodElements(RIGHT_METHOD_OPERATION).get(2));
+	}
+
+	@Test
+	void getMethodElementsReturnsEmptyListWhenNotFound() {
+		assertTrue(operationParser.getMethodElements(BAD_METHOD_OPERATION).isEmpty());
+	}
+
+}
