fed40f769c9348fee6deefcd46eb9e47d0625db1
tipikae <gilles.bd@free.fr>
2022-05-09 07:38:44 +0200

    adding CharacterOperator enum

---
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/risk2/CharacterOperator.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.tipikae.assessmentservice.risk2;
+
+import java.util.function.BiFunction;
+
+/**
+ * Enumeration of character operators.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public enum CharacterOperator implements BiFunction<Character, Character, Boolean> {
+
+	EQUALS ("=") {
+		@Override
+		public Boolean apply(Character a, Character b) {
+			return a == b;
+		}
+	},
+	DIFFERENTS ("!=") {
+		@Override
+		public Boolean apply(Character a, Character b) {
+			return a != b;
+		}
+	};
+
+	private String operator;
+	
+	private CharacterOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	public static CharacterOperator valueOfOperator(String operator) {
+	    for (CharacterOperator o : values()) {
+	        if (o.operator.equals(operator)) {
+	            return o;
+	        }
+	    }
+	    return null;
+	}
+}
