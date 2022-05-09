dffc931aba092c37f5d18a8a74cc840da439ddc1
tipikae <gilles.bd@free.fr>
2022-05-09 18:43:43 +0200

    implementing riskCalculator

--- assessment-service/src/main/java/com/tipikae/assessmentservice/risk2/RiskCalculatorImpl.java
+++ assessment-service/src/main/java/com/tipikae/assessmentservice/risk2/RiskCalculatorImpl.java
@@ -3,6 +3,7 @@
  */
 package com.tipikae.assessmentservice.risk2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -36,7 +37,7 @@ public class RiskCalculatorImpl implements IRiskCalculator {
 	private FormulaParser formulaParser;
 	
 	@Autowired
-	private IEvaluator evaluator;
+	private EvaluatorFactory evaluatorFactory;
 	
 
 	/**
@@ -45,12 +46,15 @@ public class RiskCalculatorImpl implements IRiskCalculator {
 	@Override
 	public String calculateRisk(Patient patient) throws RiskNotFoundException {
 		LOGGER.debug("calculateRisk: patientId=" + patient.getId());
+		
+		// get formulas list
 		List<Formula> formulas = formulaRepository.findAll();
 		if(formulas.isEmpty()) {
 			LOGGER.debug("calculateRisk: no formulas found.");
 			throw new RiskNotFoundException("No formulas found.");
 		}
 		
+		// search first formula valid and evaluated
 		for(Formula formula: formulas) {
 			if(!formulaValidator.validate(formula.getForm())) {
 				LOGGER.debug("calculateRisk: formula=" + formula + " is not valid.");
@@ -59,10 +63,46 @@ public class RiskCalculatorImpl implements IRiskCalculator {
 			
 			List<String> operations = formulaParser.getOperations(formula.getForm());
 			List<String> operators = formulaParser.getOperators(formula.getForm());
+			if(operations.size() != (operators.size() + 1)) {
+				LOGGER.debug("calculateRisk: operations size=" + operations.size() 
+					+ " != operators size=" + operators.size() + " + 1");
+				continue;
+			}
+			
+			List<Boolean> results = new ArrayList<>();
+			for(String operation: operations) {
+				IEvaluator evaluator = evaluatorFactory.getEvaluator(operation);
+				boolean result = false;
+				
+				try {
+					result = evaluator.evaluate(patient, operation);
+				} catch (Exception e) {
+					LOGGER.debug("calculateRisk: evaluator error: " + e.getClass().getSimpleName() 
+							+ ": " + e.getMessage());
+					break;
+				}
+				
+				if(!result) {
+					LOGGER.debug("calculateRisk: evaluator returns false for operation=" + operation);
+					break;
+				}
+				results.add(result);
+			}
+			
+			if(results.isEmpty()) {
+				LOGGER.debug("calculateRisk: no operation evaluated with formula=" + formula.getForm());
+				continue;
+			}
+			
 			
 		}
-		 
-		return null;
+		
+		LOGGER.debug("calculateRisk: no valid formula found");
+		throw new RiskNotFoundException("No valid formula found.");
+	}
+	
+	private boolean evaluateResults(List<Boolean> results, List<Boolean> operators) {
+		return false;
 	}
 
 }
--- assessment-service/src/test/java/com/tipikae/assessmentservice/unit/risk2/RiskCalculatorTest.java
+++ assessment-service/src/test/java/com/tipikae/assessmentservice/unit/risk2/RiskCalculatorTest.java
@@ -1,6 +1,10 @@
 package com.tipikae.assessmentservice.unit.risk2;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
@@ -11,9 +15,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.tipikae.assessmentservice.exception.BadOperationException2;
+import com.tipikae.assessmentservice.exception.ClientException;
+import com.tipikae.assessmentservice.exception.FieldNotFoundException2;
+import com.tipikae.assessmentservice.exception.OperatorNotFoundException2;
+import com.tipikae.assessmentservice.exception.RiskNotFoundException;
+import com.tipikae.assessmentservice.model.Formula;
 import com.tipikae.assessmentservice.model.Patient;
 import com.tipikae.assessmentservice.repository.IFormulaRepository;
+import com.tipikae.assessmentservice.risk2.EvaluatorFactory;
 import com.tipikae.assessmentservice.risk2.FormulaParser;
+import com.tipikae.assessmentservice.risk2.FormulaValidator;
 import com.tipikae.assessmentservice.risk2.IEvaluator;
 import com.tipikae.assessmentservice.risk2.RiskCalculatorImpl;
 
@@ -24,7 +36,13 @@ class RiskCalculatorTest {
 	private IFormulaRepository formulaRepository;
 	
 	@Mock
+	private FormulaValidator formulaValidator;
+	
+	@Mock
 	private FormulaParser formulaParser;
+	
+	@Mock
+	private EvaluatorFactory evaluatorFactory;
 	
 	@Mock
 	private IEvaluator evaluator;
@@ -36,7 +54,11 @@ class RiskCalculatorTest {
 	private static String operation1;
 	private static String operation2;
 	private static String operator;
-	private static String formula;
+	private static String rightForm;
+	private static String badForm;
+	private static String risk = "None";
+	private static Formula rightFormula;
+	private static Formula badFormula;
 	private static List<String> operations;
 	private static List<String> operators;
 	
@@ -47,14 +69,74 @@ class RiskCalculatorTest {
 		operation1 = "trigger = 2";
 		operation2 = "P.age < 30";
 		operator = "AND";
-		formula = operation1 + " " + operator + " " + operation2;
+		rightForm = operation1 + " " + operator + " " + operation2;
+		badForm = operation1 + operation2;
+		rightFormula = new Formula(0, risk, rightForm);
+		badFormula = new Formula(0, risk, badForm);
 		operations = List.of(operation1, operation2);
 		operators = List.of(operator);
 	}
 
 	@Test
-	void calculateRisk() {
-		
+	void calculateRiskReturnsRiskWhenOneFormulaValidAndEvaluated() 
+			throws OperatorNotFoundException2, FieldNotFoundException2, 
+			BadOperationException2, ClientException, RiskNotFoundException {
+		when(formulaRepository.findAll()).thenReturn(List.of(badFormula, rightFormula));
+		when(formulaValidator.validate(anyString())).thenReturn(false, true);
+		when(formulaParser.getOperations(anyString())).thenReturn(operations);
+		when(formulaParser.getOperators(anyString())).thenReturn(operators);
+		when(evaluatorFactory.getEvaluator(anyString())).thenReturn(evaluator, evaluator);
+		when(evaluator.evaluate(any(Patient.class), anyString())).thenReturn(true, true);
+		assertEquals(risk, riskCalculator.calculateRisk(patient));
+	}
+	
+	@Test
+	void calculateRiskThrowsRiskNotFoundExceptionWhenOneFormulaValidAndNotEvaluated() 
+			throws OperatorNotFoundException2, FieldNotFoundException2, 
+			BadOperationException2, ClientException {
+		when(formulaRepository.findAll()).thenReturn(List.of(badFormula, rightFormula));
+		when(formulaValidator.validate(anyString())).thenReturn(false, true);
+		when(formulaParser.getOperations(anyString())).thenReturn(operations);
+		when(formulaParser.getOperators(anyString())).thenReturn(operators);
+		when(evaluatorFactory.getEvaluator(anyString())).thenReturn(evaluator, evaluator);	
+		when(evaluator.evaluate(any(Patient.class), anyString())).thenReturn(false, false);
+		assertThrows(RiskNotFoundException.class, () -> riskCalculator.calculateRisk(patient));
+	}
+	
+	@Test
+	void calculateRiskThrowsRiskNotFoundExceptionWhenEvaluatorError() 
+			throws OperatorNotFoundException2, FieldNotFoundException2, 
+			BadOperationException2, ClientException {
+		when(formulaRepository.findAll()).thenReturn(List.of(badFormula, rightFormula));
+		when(formulaValidator.validate(anyString())).thenReturn(false, true);
+		when(formulaParser.getOperations(anyString())).thenReturn(operations);
+		when(formulaParser.getOperators(anyString())).thenReturn(operators);
+		when(evaluatorFactory.getEvaluator(anyString())).thenReturn(evaluator, evaluator);
+		doThrow(OperatorNotFoundException2.class)
+			.when(evaluator).evaluate(any(Patient.class), anyString());
+		assertThrows(RiskNotFoundException.class, () -> riskCalculator.calculateRisk(patient));
+	}
+	
+	@Test
+	void calculateRiskThrowsRiskNotFoundExceptionWhenNoFormula() {
+		when(formulaRepository.findAll()).thenReturn(List.of());
+		assertThrows(RiskNotFoundException.class, () -> riskCalculator.calculateRisk(patient));
+	}
+	
+	@Test
+	void calculateRiskThrowsRiskNotFoundExceptionWhenNoFormulaValid() {
+		when(formulaRepository.findAll()).thenReturn(List.of(badFormula, badFormula));
+		when(formulaValidator.validate(anyString())).thenReturn(false, false);
+		assertThrows(RiskNotFoundException.class, () -> riskCalculator.calculateRisk(patient));
+	}
+	
+	@Test
+	void calculateRiskThrowsRiskNotFoundExceptionWhenOperationsAndOperatorsSizeMismatched() {
+		when(formulaRepository.findAll()).thenReturn(List.of(badFormula, rightFormula));
+		when(formulaValidator.validate(anyString())).thenReturn(false, true);
+		when(formulaParser.getOperations(anyString())).thenReturn(operations);
+		when(formulaParser.getOperators(anyString())).thenReturn(List.of());
+		assertThrows(RiskNotFoundException.class, () -> riskCalculator.calculateRisk(patient));
 	}
 
 }
