de4e95ec564acf72ca9723292000edd3302121c8
tipikae <gilles.bd@free.fr>
2022-05-24 19:47:06 +0200

    adding new assessment service client

---
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/service/IAssessmentService.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.tipikae.mediscreenUI.service;
+
+import java.util.List;
+
+import com.tipikae.mediscreenUI.dto.AssessmentByFamilyDTO;
+import com.tipikae.mediscreenUI.dto.AssessmentByIdDTO;
+import com.tipikae.mediscreenUI.exception.NotFoundException;
+import com.tipikae.mediscreenUI.model.Assessment;
+
+/**
+ * Assessment service.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public interface IAssessmentService {
+
+	/**
+	 * Get an assessment by patientId.
+	 * @param assessmentByIdDTO AssessmentByIdDTO
+	 * @return Assessment
+	 * @throws NotFoundException
+	 */
+	//@RequestLine("GET /assess/id")
+    //@Headers("Content-Type: application/json")
+	Assessment getAssessmentById(AssessmentByIdDTO assessmentByIdDTO) throws NotFoundException;
+
+	/**
+	 * Get an assessments list by family name.
+	 * @param assessmentByFamilyDTO AssessmentByFamilyDTO
+	 * @return List
+	 */
+	//@RequestLine("GET /assess/familyName")
+    //@Headers("Content-Type: application/json")
+	List<Assessment> getAssessmentsByFamily(AssessmentByFamilyDTO assessmentByFamilyDTO);
+}
--- mediscreen-UI/src/test/java/com/tipikae/mediscreenUI/AssessmentServiceIT.java
+++ mediscreen-UI/src/test/java/com/tipikae/mediscreenUI/AssessmentServiceIT.java
@@ -8,17 +8,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.tipikae.mediscreenUI.client.IAssessmentServiceClient;
 import com.tipikae.mediscreenUI.dto.AssessmentByFamilyDTO;
 import com.tipikae.mediscreenUI.dto.AssessmentByIdDTO;
 import com.tipikae.mediscreenUI.exception.NotFoundException;
 import com.tipikae.mediscreenUI.model.Assessment;
+import com.tipikae.mediscreenUI.service.IAssessmentService;
 
 @SpringBootTest
 class AssessmentServiceIT {
 	
 	@Autowired
-	IAssessmentServiceClient assessmentClient;
+	private IAssessmentService assessmentService;
 
 	@Test
 	void test() throws NotFoundException {
@@ -47,29 +47,29 @@ class AssessmentServiceIT {
 		String inDanger = "In Danger";
 		String earlyOnset = "Early onset";
 		
-		assertTrue(assessmentClient.getAssessmentById(idNoneDTO).getMessage().endsWith(none));
-		assertTrue(assessmentClient.getAssessmentById(idBorderlineDTO).getMessage().endsWith(borderline));
-		assertTrue(assessmentClient.getAssessmentById(idInDangerDTO).getMessage().endsWith(inDanger));
-		assertTrue(assessmentClient.getAssessmentById(idEarlyOnsetDTO).getMessage().endsWith(earlyOnset));
+		assertTrue(assessmentService.getAssessmentById(idNoneDTO).getMessage().endsWith(none));
+		assertTrue(assessmentService.getAssessmentById(idBorderlineDTO).getMessage().endsWith(borderline));
+		assertTrue(assessmentService.getAssessmentById(idInDangerDTO).getMessage().endsWith(inDanger));
+		assertTrue(assessmentService.getAssessmentById(idEarlyOnsetDTO).getMessage().endsWith(earlyOnset));
 		
 		// family none
-		List<Assessment> assessmentsNone = assessmentClient.getAssessmentsByFamily(familyNoneDTO);
-		assertTrue(assessmentsNone.size() == 1);
+		List<Assessment> assessmentsNone = (List<Assessment>)assessmentService.getAssessmentsByFamily(familyNoneDTO);
+		assertTrue(assessmentsNone.size() > 0);
 		assertTrue(assessmentsNone.get(0).getMessage().endsWith(none));
 		
 		// family borderline
-		List<Assessment> assessmentsBorderline = assessmentClient.getAssessmentsByFamily(familyBorderlineDTO);
-		assertTrue(assessmentsBorderline.size() == 1);
+		List<Assessment> assessmentsBorderline = assessmentService.getAssessmentsByFamily(familyBorderlineDTO);
+		assertTrue(assessmentsBorderline.size() > 0);
 		assertTrue(assessmentsBorderline.get(0).getMessage().endsWith(borderline));
 		
 		// family inDanger
-		List<Assessment> assessmentsInDanger = assessmentClient.getAssessmentsByFamily(familyInDangerDTO);
-		assertTrue(assessmentsInDanger.size() == 1);
+		List<Assessment> assessmentsInDanger = assessmentService.getAssessmentsByFamily(familyInDangerDTO);
+		assertTrue(assessmentsInDanger.size() > 0);
 		assertTrue(assessmentsInDanger.get(0).getMessage().endsWith(inDanger));
 		
 		// family earlyOnset
-		List<Assessment> assessmentsEarlyOnset = assessmentClient.getAssessmentsByFamily(familyEarlyOnsetDTO);
-		assertTrue(assessmentsEarlyOnset.size() == 1);
+		List<Assessment> assessmentsEarlyOnset = assessmentService.getAssessmentsByFamily(familyEarlyOnsetDTO);
+		assertTrue(assessmentsEarlyOnset.size() > 0);
 		assertTrue(assessmentsEarlyOnset.get(0).getMessage().endsWith(earlyOnset));
 	}
 
