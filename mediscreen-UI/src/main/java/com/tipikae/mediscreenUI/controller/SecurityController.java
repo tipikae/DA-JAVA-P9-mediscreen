66f8f542c757afc75989c52bc579bc3f7fe2bae0
tipikae <gilles.bd@free.fr>
2022-05-26 19:26:07 +0200

    removing post login endpoint

--- mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/controller/SecurityController.java
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/controller/SecurityController.java
@@ -6,6 +6,8 @@ package com.tipikae.mediscreenUI.controller;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecurityController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SecurityController.class);
+	
 	@GetMapping("/login")
 	public String getloginForm() {
 		return "security/login";
@@ -26,6 +30,6 @@ public class SecurityController {
 	@GetMapping("/logout")
 	public String logout(HttpServletRequest request) throws ServletException {
 		request.logout();
-		return "redirect:/patient/all";
+		return "redirect:/login";
 	}
 }
