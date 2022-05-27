f7d8b901ab58d531d1b66edc2f75dff98dbb6e56
tipikae <gilles.bd@free.fr>
2022-05-27 06:31:22 +0200

    adding HttpSessionEventPublisher

--- mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/security/SecurityConfig.java
+++ mediscreen-UI/src/main/java/com/tipikae/mediscreenUI/security/SecurityConfig.java
@@ -4,11 +4,13 @@
 package com.tipikae.mediscreenUI.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 /**
  * Security configuration.
@@ -52,5 +54,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        .addLogoutHandler(logoutHandler)
 	        .deleteCookies("JSESSIONID");
     }
+	
+	@Bean
+	public HttpSessionEventPublisher httpSessionEventPublisher() {
+	    return new HttpSessionEventPublisher();
+	}
 
 }
