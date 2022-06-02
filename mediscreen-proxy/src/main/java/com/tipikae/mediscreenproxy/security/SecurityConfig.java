2c5e91a847cab996eda2ce4c37f20602b7c3649e
tipikae <gilles.bd@free.fr>
2022-06-02 15:47:10 +0200

    adding security config

--- mediscreen-proxy/src/main/java/com/tipikae/mediscreenproxy/security/SecurityConfig.java
+++ mediscreen-proxy/src/main/java/com/tipikae/mediscreenproxy/security/SecurityConfig.java
@@ -3,13 +3,22 @@
  */
 package com.tipikae.mediscreenproxy.security;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode;
+
+import reactor.core.publisher.Mono;
 
 /**
  * Security configuration.
@@ -17,32 +26,57 @@ import org.springframework.security.web.server.header.XFrameOptionsServerHttpHea
  * @version 1.0
  *
  */
-@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
-
+	
+	private static final String CLAIM_ROLES = "roles";
+	
 	/**
-	 * Build filter chain.
+	 * SecurityWebFilterChain bean.
 	 * @param http ServerHttpSecurity
-	 * @param clientRegistrationRepository ReactiveClientRegistrationRepository
 	 * @return SecurityWebFilterChain
 	 */
 	@Bean
-	public SecurityWebFilterChain springSecurityFilterChain(
-			ServerHttpSecurity http,
-			ReactiveClientRegistrationRepository clientRegistrationRepository) {
-		// Authenticate through configured OpenID Provider
-		http.oauth2Login();
-		// Also logout at the OpenID Connect provider
-		http.logout(logout -> logout.logoutSuccessHandler(
-				new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
-		// Require authentication for all requests
-		http.authorizeExchange().anyExchange().authenticated();
-		// Allow showing /home within a frame
-		http.headers().frameOptions().mode(Mode.SAMEORIGIN);
-		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-		http.csrf().disable();
-		
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+	     http
+	        .authorizeExchange(exchanges -> 
+	        		exchanges
+	        			.pathMatchers("/**").hasRole("USER")
+		             	.anyExchange().authenticated())
+	        .oauth2ResourceServer(oauth2ResourceServer ->
+	        	oauth2ResourceServer.jwt(jwt ->
+	        			jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
+	   
 		return http.build();
 	}
 	
+	// Extracts manually roles.
+	private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+	    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+	    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new GrantedAuthoritiesExtractor());
+	    
+	    return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+	}
+	
+	/**
+	 * Granted Authorities extractor.
+	 * @author tipikae
+	 * @version 1.0
+	 *
+	 */
+	class GrantedAuthoritiesExtractor implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@SuppressWarnings("unchecked")
+		@Override
+		public Collection<GrantedAuthority> convert(Jwt jwt) {
+			Collection<String> authorities = (Collection<String>) jwt.getClaims().get(CLAIM_ROLES);
+		
+		    return authorities.stream()
+		            .map(SimpleGrantedAuthority::new)
+		            .collect(Collectors.toList());
+		}
+	}
 }
