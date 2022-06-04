143d4460036448fa21a8e356ac22e69c24981da4
tipikae <gilles.bd@free.fr>
2022-06-04 17:03:17 +0200

    adding openAPI for services

--- patient-service/src/main/java/com/tipikae/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/tipikae/patientservice/PatientServiceApplication.java
@@ -1,11 +1,19 @@
 package com.tipikae.patientservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@OpenAPIDefinition(info =
+	@Info(title = "Patient Service API", 
+		version = "1.0", 
+		description = "Documentation Patient Service API v1.0"
+	)
+)
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
