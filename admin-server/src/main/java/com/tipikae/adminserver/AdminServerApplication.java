2cfd55b7032143050c13e3bb57e3d2fa09386eaa
tipikae <gilles.bd@free.fr>
2022-06-21 18:53:02 +0200

    removing discovery client

--- admin-server/src/main/java/com/tipikae/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/tipikae/adminserver/AdminServerApplication.java
@@ -3,13 +3,11 @@ package com.tipikae.adminserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
-@EnableDiscoveryClient
 @EnableAutoConfiguration
 public class AdminServerApplication {
 
