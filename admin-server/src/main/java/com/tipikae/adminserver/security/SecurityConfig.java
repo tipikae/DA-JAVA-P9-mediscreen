fc7859e3016b528e70882bc8a52e3643ce11fd5b
tipikae <gilles.bd@free.fr>
2022-06-22 18:01:20 +0200

    removing @EnableWebSecurity annotation

--- admin-server/src/main/java/com/tipikae/adminserver/security/SecurityConfig.java
+++ admin-server/src/main/java/com/tipikae/adminserver/security/SecurityConfig.java
@@ -5,9 +5,9 @@ package com.tipikae.adminserver.security;
 
 import java.util.UUID;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -20,17 +20,17 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  *
  */
 @SuppressWarnings("deprecation")
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		SavedRequestAwareAuthenticationSuccessHandler successHandler = 
 		          new SavedRequestAwareAuthenticationSuccessHandler();
-      successHandler.setTargetUrlParameter("redirectTo");
-      successHandler.setDefaultTargetUrl("/");
+		successHandler.setTargetUrlParameter("redirectTo");
+		successHandler.setDefaultTargetUrl("/");
 
-      http
+		http
           .authorizeRequests()
               .antMatchers("/assets/**").permitAll()
               .antMatchers("/login").permitAll()
